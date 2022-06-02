2915341af1c019105cdf6f809a392eba3e7387d0
tfitzpatrick0 <tim.fitz830@gmail.com>
2022-06-02 14:32:54 -0700

    navbar and css

---
+++ src/Intro.js
@@ -0,0 +1,11 @@
+function Intro() {
+  return (
+    <div>
+      <h1>Tim Fitzpatrick</h1>
+      <h2>Computer Science Student</h2>
+      <h3>at the University of Notre Dame</h3>
+    </div>
+  );
+}
+
+export default Intro;
---
+++ src/Navbar.js
@@ -0,0 +1,41 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="projects.html">Projects</a>
+        </li>
+        <li>
+          <a href="about.html">About</a>
+        </li>
+        <li>
+          <a href="contact.html">Contact</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/tfitzpatrick3/">
+            <span class="fab fa-linkedin" aria-hidden="true"></span>
+            <span class="sr-only">LinkedIn</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/tfitzpatrick0">
+            <span class="fab fa-github-square" aria-hidden="true"></span>
+            <span class="sr-only">Github</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://engine.mybookie.ag/sports/home?first_page=1"
+            class="button"
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
