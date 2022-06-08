5d606309e90e3fcaa17cd83790946435502652df
tfitzpatrick0 <tim.fitz830@gmail.com>
2022-06-07 23:56:04 -0700

    hero looking good

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,19 +1,38 @@
 /** @jsxImportSource @emotion/react */
 import "./style.css";
+import Link from "../shared/Link";
 import { colors } from "../../constants.js";
-import ndPic from "../../images/nd_winter.jpeg";
+import ndPic from "../../images/nd_winter.png";
 
 function Intro() {
   return (
     <div className="hero-container" css={{ backgroundColor: colors.honey }}>
       <div className="hero-text-block" css={{ backgroundColor: colors.orange }}>
-        <h1>Tim Fitzpatrick</h1>
-        <h2>Computer Science Student</h2>
-        <h3>at the University of Notre Dame</h3>
+        <h1 className="hero-name" css={{ color: colors.white }}>
+          Tim Fitzpatrick
+        </h1>
+        <h2 className="hero-subtext" css={{ color: colors.white }}>
+          <Link
+            href="https://cse.nd.edu/"
+            primary={colors.white}
+            hover={colors.skyBlue}
+          >
+            Computer Science
+          </Link>
+          <br />
+          at
+          <br />
+          <Link
+            href="https://nd.edu/"
+            primary={colors.white}
+            hover={colors.skyBlue}
+          >
+            The University of Notre Dame
+          </Link>
+        </h2>
       </div>
-      <div className="hero-background">
-        <div className="hero-background-image"></div>
-        <img src={ndPic} alt="ND" />
+      <div className="img-container">
+        <img className="background-img" src={ndPic} alt="ND" />
       </div>
     </div>
   );
