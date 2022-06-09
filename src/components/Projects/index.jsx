1fb1b26aedcfcd1742aa433abde8cb4b2cca8b29
tfitzpatrick0 <tim.fitz830@gmail.com>
2022-06-08 23:31:04 -0700

    projects section looking good

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -2,8 +2,8 @@
 import "./style.css";
 import { colors } from "../../constants.js";
 
-// import facerec from "../../images/facerec.png";
-// import filesys from "../../images/filesys.png";
+import facerec from "../../images/facerec.png";
+import filesys from "../../images/filesys.png";
 import muse from "../../images/muse.png";
 
 function Projects() {
@@ -25,22 +25,22 @@ function Projects() {
         <div className="project-info-regular">
           <div className="project-info-text">
             <h4 css={{ color: colors.black }}>Databases Project</h4>
-            <h3 css={{ color: colors.blue }}>Muse Web App</h3>
+            <h2 css={{ color: colors.blue }}>Muse Web App</h2>
             <p
               className="project-overlay"
               css={{ color: colors.white, backgroundColor: colors.orange }}
             >
-              Description of the project...{" "}
               <a href="https://github.com/tfitzpatrick0/Databases-MuseWebApp">
-                Check it out
+                Project link
               </a>
-              . Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              . Description - Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum.
             </p>
             <h4 css={{ color: colors.black }}>Technologies used include:</h4>
             <ul css={{ color: colors.black }}>
@@ -50,7 +50,70 @@ function Projects() {
               <li>MySQL</li>
             </ul>
           </div>
-          <img className="project-image" src={muse} alt="Lil Muse Code." />
+          <img className="project-image" src={muse} alt="Muse Code." />
+        </div>
+        <div className="project-info-reverse">
+          <div className="project-info-text">
+            <h4 css={{ color: colors.black }}>Computer Vision Project</h4>
+            <h2 css={{ color: colors.blue }}>Face Recognition System</h2>
+            <p
+              className="project-overlay"
+              css={{ color: colors.white, backgroundColor: colors.orange }}
+            >
+              <a href="https://github.com/tfitzpatrick0/CV-FaceRecognition">
+                Project link
+              </a>
+              . Description - Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum.
+            </p>
+            <h4 css={{ color: colors.black }}>Technologies used include:</h4>
+            <ul css={{ color: colors.black }}>
+              <li>Python</li>
+              <li>Machine Learning</li>
+            </ul>
+          </div>
+          <img
+            className="project-image"
+            src={facerec}
+            alt="Face Recognition Code."
+          />
+        </div>
+        <div className="project-info-regular">
+          <div className="project-info-text">
+            <h4 css={{ color: colors.black }}>Operating Systems Project</h4>
+            <h2 css={{ color: colors.blue }}>Simple File System</h2>
+            <p
+              className="project-overlay"
+              css={{ color: colors.white, backgroundColor: colors.orange }}
+            >
+              <a href="href=https://github.com/tfitzpatrick0/OS-FileSystem">
+                Project link
+              </a>
+              . Description - Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum.
+            </p>
+            <h4 css={{ color: colors.black }}>Technologies used include:</h4>
+            <ul css={{ color: colors.black }}>
+              <li>C</li>
+            </ul>
+          </div>
+          <img
+            className="project-image"
+            src={filesys}
+            alt="File System Code."
+          />
         </div>
       </div>
     </div>
