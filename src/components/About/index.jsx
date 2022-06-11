59d8880032c107baac0a1b5c1a903dd4334e0fdf
tfitzpatrick0 <tim.fitz830@gmail.com>
2022-06-10 22:11:39 -0700

    about header looks good

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import "./style.css";
-// import Link from "../shared/Link";
+import Link from "../shared/Link";
 import { colors } from "../../constants.js";
 
 function About() {
@@ -12,18 +12,21 @@ function About() {
     >
       <div className="about-header-block">
         <div
-          className="about-linkedin"
-          css={{ color: colors.white, backgroundColor: colors.blue }}
-        >
-          <h1 className="about-header-text">
-            <span className="fab fa-linkedin" aria-hidden="true"></span>{" "}
-          </h1>
-        </div>
-        <div
           className="about-header"
           css={{ color: colors.white, backgroundColor: colors.blue }}
         >
           <h1 className="about-header-text">About Me</h1>
+        </div>
+        <div className="about-link" css={{ backgroundColor: colors.blue }}>
+          <div className="about-link-text">
+            <Link
+              href="https://www.linkedin.com/in/tfitzpatrick3/"
+              primary={colors.white}
+              hover={colors.orange}
+            >
+              <span className="fab fa-linkedin" aria-hidden="true"></span>
+            </Link>
+          </div>
         </div>
       </div>
       <div className="about-info-block">
